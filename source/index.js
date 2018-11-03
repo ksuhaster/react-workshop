140b821b06b7ca260300d1c70a0b22bd7982b42b
Oksana Lobko <ksuhaster@gmail.com>
2018-11-03 20:06:05 +0200

    my changes

--- source/index.js
+++ source/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom';
 // Theme
 import './theme/init';
 
-const start = <h1 className = 'welcome'>Добро пожаловать!</h1>;
+import { Kinoafisha } from './pages/Kinoafisha';
 
-ReactDOM.render(start, document.getElementById('app'));
+ReactDOM.render(<Kinoafisha/>, document.getElementById('app'));
